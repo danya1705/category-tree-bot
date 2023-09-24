ad376ae9a9d60a69cf32ea95d6e46d2be9c2ee3d
danya1705 <danya17@gmail.com>
2023-09-24 10:12:48 +0300

    initial commit

---
+++ src/main/java/com/github/danya1705/categorytreebot/CategoryTreeBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.danya1705.categorytreebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryTreeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryTreeBotApplication.class, args);
+    }
+
+}
